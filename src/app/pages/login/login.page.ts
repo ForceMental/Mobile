e38825bea50e5bddc9f5cae3d4d1990c97168c57
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2023-10-05 00:03:43 -0300

    Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -19,10 +19,10 @@ export class LoginPage implements OnInit {
     try {
       const token = await this.authService.login();
       console.log("Token obtenido: ", token);
-      // Navega al siguiente componente o realiza alguna otra acción.
+
     } catch (error) {
       console.error("Error durante el inicio de sesión: ", error);
-      // Maneja el error como consideres necesario, por ejemplo, muestra un mensaje de error al usuario.
+
     }
   }
 
